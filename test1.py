d6926064521dbc0462fef465fde7778afaec1804
Proverbs13 <ichan0318@naver.com>
2023-08-14 18:08:05 +0900

    방문풀 24일차

--- test1.py
+++ test1.py
@@ -3507,30 +3507,35 @@
 
 
 
-# 정수 삼각형
-import sys
-input=sys.stdin.readline
-N=int(input())
-l=[]
-dp=[[] for i in range(N)]
+# # 정수 삼각형
+# import sys
+# input=sys.stdin.readline
+# N=int(input())
+# l=[]
+# dp=[[] for i in range(N)]
+#
+# for i in range(1,N+1):
+#     for j in range(i):
+#         dp[i-1].append(0)
+#
+# for i in range(N):
+#     l.append((list(map(int,input().split()))))
+#
+# dp[0][0]=l[0][0]
+#
+# for i in range(1,N):
+#     for j in range(len(l[i])):
+#         if j == 0:
+#             dp[i][j]=dp[i-1][j]+l[i][j]
+#         elif j==len(l[i])-1:
+#             dp[i][j]=dp[i-1][j-1]+l[i][j]
+#         else:
+#             dp[i][j]=l[i][j]+max(dp[i-1][j-1],dp[i-1][j])
+#
+# print(max(dp[N-1]))
 
-for i in range(1,N+1):
-    for j in range(i):
-        dp[i-1].append(0)
 
-for i in range(N):
-    l.append((list(map(int,input().split()))))
-
-dp[0][0]=l[0][0]
-
-for i in range(1,N):
-    for j in range(len(l[i])):
-        if j == 0:
-            dp[i][j]=dp[i-1][j]+l[i][j]
-        elif j==len(l[i])-1:
-            dp[i][j]=dp[i-1][j-1]+l[i][j]
-        else:
-            dp[i][j]=l[i][j]+max(dp[i-1][j-1],dp[i-1][j])
-
-print(max(dp[N-1]))
+#사파리월드
+N,M=map(int,input().split())
+print(abs(N-M))
 
